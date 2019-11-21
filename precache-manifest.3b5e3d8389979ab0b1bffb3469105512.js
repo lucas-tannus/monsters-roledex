5475deeb2e055d5879a047723a965032dcbbc9f7
Lucas Tannús <lucastannus@softbox.com.br>
2019-11-21 13:57:28 -0300

    Updates

---
+++ precache-manifest.3b5e3d8389979ab0b1bffb3469105512.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db317ce18df83907f619b3f089b6a48",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "80e5b04881c36089ea24",
+    "url": "/monsters-rolodex/static/css/main.78d20796.chunk.css"
+  },
+  {
+    "revision": "ba41f8274473a4edf0c7",
+    "url": "/monsters-rolodex/static/js/2.63450026.chunk.js"
+  },
+  {
+    "revision": "80e5b04881c36089ea24",
+    "url": "/monsters-rolodex/static/js/main.fad345be.chunk.js"
+  },
+  {
+    "revision": "8938a4cfc835d08eb279",
+    "url": "/monsters-rolodex/static/js/runtime-main.475f16ed.js"
+  }
+]);
